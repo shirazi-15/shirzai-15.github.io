9a98c013cb480248fba4e75f9a9640954434a630
Shirazi7019505 <7019505@spsd.sk.ca>
2025-09-12 14:11:18 -0600

    have a good weekend

--- 02 user events/sketch.js
+++ 02 user events/sketch.js
@@ -4,23 +4,70 @@
 // - describe what you did to take this project "above and beyond"
 
 // Global variable Here
-let circleColor = false;
+// can only wrok with "simple" data in this
+// section of code
 
+let circleColor = false;
+let currentColor = "white";
+
+let x; let y;
+let tsize = 50;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400);  
+  x = width/2;        
+  y = 300;           
 }
 
 function draw() {
   background(220);
   challenge();
+  movement();
+  rect(x, 300, 60, 30);
+  mouseReport();
+}
+
+function movement(){
+  // check for keyboard presses in each frame
+  // and move the rect accordingly
+  // if(keyCode === RIGHT_ARROW && keyIsPressed){
+  //   x = x + 5
+  // }
+  // else if(keyCode === LEFT_ARROW && keyIsPressed){
+  //   x = x - 5
+  // }
+
+  if (keyIsDown(UP_ARROW)) y = y - 5;
+  if (keyIsDown(DOWN_ARROW)) y = y + 5;
+  if (keyIsDown(RIGHT_ARROW)) x = x + 5;
+  if (keyIsDown(LEFT_ARROW)) x = x - 5;
+  
+}
+
+function mouseReport(){
+  // inspect some of thr build_ins
+  // for working with some mouse
+  let src = mouseX + ", " + mouseY + ", " + mouseIsPressed + ", " + mouseButton;
+  text(src, mouseX, mouseY);
+
+  
 }
 
 function keyPressed(){
   // this is special EVENT function, gets
   // automatically called anytime a keyboard
+
+
   // button is pressed
   print("Key is pressed");
+  if(key === "g"){
+    currentColor = "green";
+  }
+  else if(keyCode === CONTROL){
+    currentColor = "blue";
+
+  }
+
   circleColor = !circleColor
 }
 
@@ -30,7 +77,7 @@ function challenge(){
   noFill();
   
   if(circleColor === true){
-    fill(255, 0, 0);
+    fill(currentColor);
   }
   // circle 5 
   circle(height, width, 50);
