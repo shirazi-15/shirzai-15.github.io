837de6fb18b9216338dcf6d2d324d76b55cb4067
Shirazi7019505 <7019505@spsd.sk.ca>
2025-09-16 13:35:11 -0600

    shapes exercise done

--- shapes exercise/sketch.js
+++ shapes exercise/sketch.js
@@ -9,27 +9,32 @@
 let cent_x;
 let cent_y;
 let scale;
+let headSize;
 
 function setup() {
   createCanvas(400, 400);
   cent_x = width / 2;
   cent_y = height / 2;
+  slider = createSlider(1, 3, 0.1);
+  slider.position(10, 10);
+  slider.size(80);
 }
 
 function draw() {
   background(220);
-  Aline();
+  headSize = slider.value();
+  Alien();
 }
 
-function Aline(){
+function Alien(){
   noStroke()
   fill(153, 255, 153);
   rectMode(CENTER);
-  rect(cent_x, cent_y, 80, 80, 50, 50, 0, 0)
-  rect(cent_x - 35, cent_y + 55, 10, 30, 0, 0, 5, 5)
-  rect(cent_x + 35, cent_y + 55, 10, 30, 0, 0, 5, 5)
+  rect(cent_x, cent_y, 80 * headSize, 80 * headSize, 50* headSize, 50 * headSize, 0, 0);
+  rect(cent_x - 35 * headSize, cent_y + 55 * headSize, 10 * headSize, 30* headSize, 0, 0, 5* headSize, 5* headSize);
+  rect(cent_x + 35 * headSize, cent_y + 55 * headSize, 10 * headSize, 30 * headSize, 0, 0, 5 * headSize, 5* headSize);
   fill(0, 0, 0);
-  circle(cent_x + 15, cent_y - 5, 7);
-  circle(cent_x - 15, cent_y - 5, 7);
-  rect(cent_x, cent_y+ 10, 20, 1);
+  circle(cent_x + 15 * headSize, cent_y - 5 * headSize, 7 * headSize);
+  circle(cent_x - 15 * headSize, cent_y - 5 * headSize, 7 * headSize);
+  rect(cent_x, cent_y+ 10 * headSize, 20 * headSize, 1 * headSize);
 }
