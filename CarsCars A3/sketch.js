0a9378aa1ee20cb7ec2259614fd5f834d480476e
Shirazi7019505 <7019505@spsd.sk.ca>
2025-10-27 14:13:04 -0600

    not yet

--- CarsCars A3/sketch.js
+++ CarsCars A3/sketch.js
@@ -15,14 +15,14 @@ let lLift;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   uplift = height*0.15;
-  lLift = height*0.10;
+  lLift = height*0.15;
   for(let i = 0; i < 10; i++){
   e = new Vehicle(width/4, random(height*0.03 + uplift, height/2 - lLift), 0);
   eastbound.push(e);
   }
 
   for(let i = 0; i < 10; i++){
-    w = new Vehicle(width/4, random(height/2 + uplift, height - (height*0.03 + lLift)), 1);
+    w = new Vehicle(width/4, random(height/2 + lLift, height - (height*0.015 + lLift)), 1);
     westbound.push(w);
   }
   // mytrafficLight = new TrafficLight(width/2, height*0.03);
@@ -99,7 +99,7 @@ class Vehicle{
     }
    else{
       strokeWeight(2);
-      line(this.x + fixXT/2, this.y - fixXT/2, this.x + fixXT/2, this.y + fixXT/2);
+      line(this.x + fixXT/8, this.y - fixXT/3, this.x + fixXT/8, this.y + fixXT/3);
       noStroke();
    }
   }
@@ -176,37 +176,39 @@ class TrafficLight{
   // 1. Constructor 
   constructor(x, y){
     this.x = x; this.y = y;
-    this.lightColor;
+    this.lightColor = "green";
     this.trafficTime = 0;
   }
 
   // 2. methods
   display(){
-    fill(0);
-    rect(this.x, this.y + 5, width*0.15, height*0.11);
-    let ficCx = width*0.04;
-    let fixCy = height*0.02;
-    fill()
-    fill(this.lightColor)
-    circle(this.x + ficCx, this.y + fixCy, width*0.04);
-   
-
-  }
-
-  redLight(){
-
-  }
-
-  greenLight(){
-
-  }
-  
-  update(){
-    if(keyCode === ' '){
-      this.lightColor = "red"
+    if(this.lightColor === "green"){
+      fill("green")
     }
     else{
-      this.lightColor = "green"
+      fill("red")
+    }
+
+    circle(this.x, this.y, 5)
+  }
+
+  turnRed(){
+    if(this.lightColor === "green"){{{{{
+      this.lightColor = "red";
+      this.trafficTime = 120;
+    }}}}}
+
+    
+  }
+
+  
+  update(){
+    if(this.lightColor === "red"){
+      this.trafficTime--;
+      if(this.trafficTime <= 0){
+        this.lightColor = "green";
+      }
     }
   }
+
 }
