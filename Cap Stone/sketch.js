09ff61955272e2460dac7289cf1461d3d5017e10
shirazi-15 <7019505@spsd.sk.ca>
2025-12-02 21:30:23 -0600

    cap 1
    worked on sprit and looked at gravity

--- Cap Stone/sketch.js
+++ Cap Stone/sketch.js
@@ -3,16 +3,31 @@
 // 2 Dec to 
 
 // Global Varible
-let bg; let p_up
-
+let bg; let pUp; let pDw;
+let b1; let b2;
 
 async function setup() {
+  bg = await loadImage("assets.FB/background.png");
+  b1 = await loadImage("assets.FB/bird.png");
+  b2 = await loadImage("assets.FB/bird2.png");
+  pUp = await loadImage("assets.FB/pipe1.png");
+  pDw = await loadImage("assets.FB/pipe2.png");
 
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
-  background(220);
+function draw(){
+  background(bg);
 }
 
+function player(){
+  switch(ani){
+    case 0:
+      b1;
+      break;
 
+    case 1:
+      b2;
+      break;
+  }
+}
