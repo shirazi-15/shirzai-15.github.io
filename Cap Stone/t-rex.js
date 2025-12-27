e27e17768ffafbbb44835d385c23cfb33af0f8e3
shirazi-15 <7019505@spsd.sk.ca>
2025-12-27 17:52:05 -0600

    cap
    Have fixed all the bugs and added clouds. Should be finished in a day or two.

--- Cap Stone/t-rex.js
+++ Cap Stone/t-rex.js
@@ -3,17 +3,18 @@
 // 13 Dec
 
 // Global Variables       
-let groundHeight = 20; let groundOffset = 0; 
+let groundHeight = 25; let groundOffset = 0; 
 let groundSpeed = 8; let lineSpacing = 4; 
-let gameState = false; let run1, run2, idel, ob1;
+let gameState = false; let run1, run2, idel, ob1, clouds;
 let obstacles = []; let nextSpawn = 0;
-let player; 
+let player; let cloudsArr = [];
 
 function preload(){
   run1 = loadImage("assets.Tx/run1.png");
   run2 = loadImage("assets.Tx/run2.png");
   idel = loadImage("assets.Tx/idel.png");
   ob1 = loadImage("assets.Tx/ob1.png");
+  clouds = loadImage("assets.Tx/clouds.png");
 }
 
 function setup(){
@@ -22,9 +23,14 @@ function setup(){
 }
 
 function draw(){
-  background(255);
+  background(245);
   
   if(gameState === true){
+    for (let c of cloudsArr) {
+    c.move();
+    c.display();
+    }
+
     drawGround();
     player.update();
     player.display();
@@ -33,7 +39,7 @@ function draw(){
 
     if (frameCount > nextSpawn){
       obstacles.push(new Obstacle(width, groundSpeed, ob1)); 
-      nextSpawn = frameCount + random(60, 120);
+      nextSpawn = frameCount + random(40, 90);
     }
 
     for (let i = obstacles.length - 1; i >= 0; i--){
@@ -87,11 +93,38 @@ function keyPressed(){
 }
 
 function restartGame(){
+  // Resets Every thing ensuring that the game starts smoothly
   groundSpeed = 8;
   obstacles = [];
   nextSpawn = frameCount + 60;
   player.y = height - groundHeight - player.h;
   player.vy = 0;
+  // Create 3-4 clouds at random positions
+  for (let i = 0; i < 4; i++) {
+    cloudsArr.push(new Cloud(random(width), random(50, 200)));
+  }
+
+  let attempts = 0;
+  let maxClouds = 5;
+
+  while (cloudsArr.length < maxClouds && attempts < 100){
+    let newCloud = new Cloud(random(width, width * 2), random(50, 250), random(1, 2.5));
+
+    let overlapping = false;
+    for (let existing of cloudsArr){
+      if (newCloud.overlaps(existing)){
+        overlapping = true;
+        break;
+      }
+    }
+
+    if (!overlapping) {
+      cloudsArr.push(newCloud);
+    }
+    attempts++; // Safety break to prevent infinite loops
+  }
+
+
   gameState = true;
   loop(); 
 }
@@ -100,25 +133,34 @@ function drawGround(){
   stroke(150);
   strokeWeight(2);
   let groundY = height - groundHeight;
+  
+  // Draw the main solid ground line
   line(0, groundY, width, groundY);
   
   groundOffset -= groundSpeed;
-  if(groundOffset <= -width) groundOffset = 0;
   
-  randomSeed(1); 
-  for(let x = groundOffset; x < width + 100; x += 60){
-    let yOffset = floor(random(1, 4)) * lineSpacing;
-    let dashY = groundY + yOffset;
+  let spacing = 40;
+  
+  let relativeOffset = groundOffset % spacing;
+
+  for(let x = relativeOffset; x < width + spacing; x += spacing){
+    
+    let worldX = x - groundOffset;
+    let dashId = floor(worldX / spacing);
+    
+    let fixedYOffset = ((dashId % 5) + 1) * lineSpacing;    
+    let dashY = groundY + fixedYOffset;
+    
     line(x, dashY, x + 20, dashY);
   }
 }
 
-class Dino {
+class Dino{
   constructor(x){
     this.x = x;
     this.w = 45; // Hitbox width
     this.h = 50; // Hitbox height
-    this.y = height - groundHeight - this.h;
+    this.y = height - groundHeight/2 - this.h;
     this.vy = 0;
     this.g = 0.8;
     this.jumpForce = -15;
@@ -128,8 +170,8 @@ class Dino {
   update(){
     this.vy += this.g;
     this.y += this.vy;
-    if(this.y >= height - groundHeight - this.h){
-      this.y = height - groundHeight - this.h;
+    if(this.y >= height - groundHeight/2 - this.h){
+      this.y = height - groundHeight/2 - this.h;
       this.vy = 0;
       this.onGround = true;
     }
@@ -148,7 +190,7 @@ class Dino {
   }
 }
 
-class Obstacle {
+class Obstacle{
   constructor(x, speed, img){
     this.img = img;
     this.speed = speed;
@@ -156,7 +198,7 @@ class Obstacle {
     this.h = 50;
     let aspect = this.img.width / this.img.height;
     this.w = this.h * aspect;
-    this.y = height - groundHeight - this.h;
+    this.y = height - groundHeight/4 - this.h;
   }
 
   move(){
@@ -176,5 +218,32 @@ class Obstacle {
       this.x + 4, this.y + 4, this.w - 8, this.h - 8,
       dino.x + 4, dino.y + 4, dino.w - 8, dino.h - 8
     );
+  }
+}
+
+class Cloud {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.speed = 1.5;
+    this.w = 90; 
+    this.h = 50;
+  }
+
+  move(){
+    this.x -= this.speed;
+    if (this.x < -this.w) {
+      this.x = width + random(50, 400);
+    }
+  }
+
+  display(){
+    image(clouds, this.x, this.y, this.w, this.h);
+  }
+
+  overlaps(other) {
+    let padding = 40; 
+    return (abs(this.x - other.x) < this.w + padding && 
+            abs(this.y - other.y) < this.h + padding);
   }
 }
